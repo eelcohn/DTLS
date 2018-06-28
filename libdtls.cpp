b82654e6efa59f7e3e496a554e1f3332e0e9e11d
EelCapone <eelcapone@reesdice.spectre.local>
2018-06-28 08:56:20 +0200

    Initial commit

---
+++ libdtls.cpp
@@ -0,0 +1,273 @@
+/* libdtls.cpp - DTLS (Datagram TLS) library using OpenSSL
+   written by Eelco Huininga - 2018                        */
+
+#include <cstring>		/* memset() */
+#include <arpa/inet.h>		/* sockaddr_in, AF_INET, SOCK_DGRAM, socket(), htons(), INADDR_ANY, htonl(), inet_pton(), SOL_SOCKET, SO_REUSEADDR, setsockopt(), bind() */
+#include <openssl/ssl.h>
+#include <openssl/engine.h>	/* ENGINE_cleanup(), ERR_free_strings() */
+#include <openssl/conf.h>	/* CONF_modules_unload() */
+
+const int		SOCKET_TIMEOUT_S = 1;
+const int		SOCKET_TIMEOUT_US = 0;
+extern const char	*SSL_CIPHERS;
+extern const char	*SSL_SIGALGS;
+extern const char	*DHFILE;
+extern const char	*CA;
+extern const char	*CERT;
+extern const char	*PRIVKEY;
+extern const char	reply[];
+
+
+namespace libdtls {
+	/* Initialize OpenSSL and create a new SSL context */
+	SSL_CTX* ssl_initialize(void) {
+		int		codes;
+		FILE		*fp_dhfile;
+		SSL_CTX		*ctx;
+		DH		*dh;
+
+		/* Initialize OpenSSL */
+		SSL_library_init();
+		SSL_load_error_strings();
+		ERR_load_BIO_strings();
+		OpenSSL_add_all_algorithms();
+
+		/* Set OpenSSL context to DTLS 1.2 */
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
+		if ((ctx = SSL_CTX_new(DTLSv1_2_server_method())) == NULL) {
+			printf("libdtls::ssl_initialize: SSL_CTX_new failed.\n");
+			exit(1);
+		}
+#else
+		if ((ctx = SSL_CTX_new(DTLS_server_method())) == NULL) {
+			printf("libdtls::ssl_initialize: SSL_CTX_new(DTLS_server_method) failed.\n");
+			exit(1);
+		}
+		if (SSL_CTX_set_min_proto_version(ctx, DTLS1_2_VERSION) != 1) {
+			printf("libdtls::ssl_initialize: Warning: dtls_InitContextFromKeystore() cannot set minimum supported protocol version\n");
+		} 
+#endif
+
+		/* Set available ciphers */
+		if (SSL_CIPHERS != NULL) {
+			if ((SSL_CTX_set_cipher_list(ctx, SSL_CIPHERS)) != 1) {
+				printf("libdtls::ssl_initialize: SSL_CTX_set_ciper_list() failed.\n");
+				exit(1);
+			}
+		}
+
+		/* Set available signature algorithms */
+		if (SSL_SIGALGS != NULL) {
+			if ((SSL_CTX_set1_sigalgs_list(ctx, SSL_SIGALGS)) != 1) {
+				printf("libdtls::ssl_initialize: SSL_CTX_set1_sigalgs_list() failed.\n");
+				exit(1);
+			}
+		}
+
+		/* Set temporary DH parameters */
+		SSL_CTX_set_options(ctx, SSL_OP_SINGLE_DH_USE);
+		if ((fp_dhfile = fopen(DHFILE, "r")) != NULL) {
+			dh = PEM_read_DHparams(fp_dhfile, NULL, NULL, NULL);
+			fclose(fp_dhfile);
+
+			codes = 0;
+			if (!DH_check(dh, &codes)) {
+				printf("libdtls::ssl_initialize: DH_check() failed with code %i.\n", codes);
+				exit(1);
+			}
+			if (SSL_CTX_set_tmp_dh (ctx, dh) != 1) {
+				printf("libdtls::ssl_initialize: SSL_CTX_set_tmp_dh() failed.\n");
+				exit(1);
+			}
+			DH_free(dh);
+		}
+
+		/* Load CA certificates */
+		if (CA != NULL) {
+			if (SSL_CTX_load_verify_locations(ctx, CA, 0) != 1) {
+				printf("libdtls::ssl_initialize: Error loading %s, please check the file.\n", CA);
+				exit(1);
+			}
+		}
+		/* Load server certificates */
+		if (SSL_CTX_use_certificate_file(ctx, CERT, SSL_FILETYPE_PEM) != 1) {
+			printf("libdtls::ssl_initialize: Error loading %s, please check the file.\n", CERT);
+			exit(1);
+		}
+		/* Load server Keys */
+		if (SSL_CTX_use_PrivateKey_file(ctx, PRIVKEY, SSL_FILETYPE_PEM) != 1) {
+			printf("libdtls::ssl_initialize: Error loading %s, please check the file.\n", PRIVKEY);
+			exit(1);
+		}
+		if (SSL_CTX_check_private_key(ctx) != 1) {
+			printf("libdtls::ssl_initialize: Private key %s not valid.\n", PRIVKEY);
+			exit(1);
+		}
+
+//		SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+		return ctx;
+	}
+
+	/* Clean up the SSL context and shutdown the SSL engine */
+	void ssl_cleanup(SSL_CTX *ctx) {
+		SSL_CTX_free(ctx);
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
+		ERR_remove_thread_state(NULL);
+#endif
+		ENGINE_cleanup();
+		CONF_modules_unload(1);
+		ERR_free_strings();
+		EVP_cleanup();						// Cleanup for OpenSSL_add_all_algorithms();
+	//	sk_SSL_COMP_free(SSL_COMP_get_compression_methods());
+		CRYPTO_cleanup_all_ex_data();
+	}
+
+	/* Print an user readable SSL error */
+	void ssl_print_error(const char *function, int sslerrno) {
+		printf("sslerrno:%i - ERR_get_error:%lu - errno:%i %s\n", sslerrno, ERR_get_error(), errno, strerror(errno));
+		printf("%s\n", ERR_error_string(ERR_get_error(), NULL));
+		printf("%s failed with error ", function);
+
+
+		switch (sslerrno) {
+			case SSL_ERROR_NONE :
+				printf("SSL_ERROR_NONE.\n");
+				break;
+
+			case SSL_ERROR_ZERO_RETURN :
+				printf("SSL_ERROR_ZERO_RETURN.\n");
+				break;
+
+			case SSL_ERROR_WANT_READ :
+				printf("SSL_ERROR_WANT_READ.\n");
+				break;
+
+			case SSL_ERROR_WANT_WRITE :
+				printf("SSL_ERROR_WANT_WRITE.\n");
+				break;
+
+			case SSL_ERROR_WANT_CONNECT :
+				printf("SSL_ERROR_WANT_CONNECT.\n");
+				break;
+
+			case SSL_ERROR_WANT_ACCEPT :
+				printf("SSL_ERROR_WANT_ACCEPT.\n");
+				break;
+
+			case SSL_ERROR_WANT_X509_LOOKUP :
+				printf("SSL_ERROR_WANT_X509_LOOKUP.\n");
+				break;
+
+	#if (OPENSSL_VERSION_NUMBER >= 0x10100000L)
+			case SSL_ERROR_WANT_ASYNC :
+				printf("SSL_ERROR_WANT_ASYNC.\n");
+				break;
+	#endif
+
+			case SSL_ERROR_WANT_ASYNC_JOB :
+				printf("SSL_ERROR_WANT_ASYNC_JOB.\n");
+				break;
+
+	#if (OPENSSL_VERSION_NUMBER >= 0x10101000L)
+			case SSL_ERROR_WANT_CLIENT_HELLO_CB :
+				printf("SSL_ERROR_WANT_CLIENT_HELLO_CB.\n");
+				break;
+	#endif
+
+			case SSL_ERROR_SYSCALL :
+				printf("SSL_ERROR_SYSCALL.\n");
+				break;
+
+			case SSL_ERROR_SSL :
+				printf("SSL_ERROR_SSL.\n");
+				break;
+
+			default :
+				printf("unknown error.\n");
+				break;
+		}
+	}
+
+	int create_socket(int family, char *address, unsigned short port) {
+		int sock, reuseconn, result;
+		struct timeval timeout;
+
+		/* Create a UDP/IP socket */
+		if ((sock = socket(family, SOCK_DGRAM, 0)) < 0 ) {
+			printf("create_socket: socket() failed.\n");
+			return(-1);
+		}
+
+		/* Set socket to allow multiple connections */
+		reuseconn = 1;
+		if ((result = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuseconn, sizeof(reuseconn))) < 0) {
+			printf("create_socket: setsockopt(SO_REUSEADDR) failed.\n");
+			return(-2);
+		}
+
+		/* Set timeout on socket to prevent recvfrom from blocking execution */
+		timeout.tv_sec = SOCKET_TIMEOUT_S;
+		timeout.tv_usec = SOCKET_TIMEOUT_US;
+		if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+			printf("create_socket: Error setting timeout on socket");
+			return(-3);
+		}
+
+		switch (family) {
+			case AF_INET :
+				struct sockaddr_in addr;
+
+				/* Set IP header */
+				memset((char *)&addr, 0, sizeof(addr));
+				addr.sin_family = AF_INET;
+				addr.sin_port = htons(port);
+				if (address == NULL)
+					addr.sin_addr.s_addr = htonl(INADDR_ANY);
+				else {
+					if (inet_pton(AF_INET, address, (void *)&addr.sin_addr.s_addr) != 1) {
+						printf("create_socket: Invalid IPv4 address");
+						return(-4);
+					}
+				}
+
+				/* Bind Socket */
+				if (bind(sock, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+					printf("create_socket: IPv4 Unable to bind");
+					return(-5);
+				}
+
+				break;
+
+			case AF_INET6 :
+				struct sockaddr_in6 addr6;
+
+				/* Set IP header */
+				memset((char *)&addr6, 0, sizeof(addr6));
+				addr6.sin6_family = AF_INET6;
+				addr6.sin6_port = htons(port);
+				addr6.sin6_scope_id = 0;
+				if (address == NULL)
+					addr6.sin6_addr = in6addr_any;
+				else {
+					if (inet_pton(AF_INET6, address, (void *)&addr6.sin6_addr.s6_addr) != 1) {
+						printf("create_socket: Invalid IPv6 address");
+						return(-6);
+					}
+				}
+
+				/* Bind Socket */
+				if (bind(sock, (struct sockaddr*) &addr6, sizeof(addr6)) < 0) {
+					printf("ccreate_socket: IPv6 Unable to bind");
+					return(-7);
+				}
+
+				break;
+
+			default :
+				printf("ccreate_socket: unknown family");
+				exit(-8);
+		}
+
+		return(sock);
+	}
+}
---
+++ server-dtls.cpp
@@ -0,0 +1,172 @@
+/* server-dtls.cpp - Simple DTLS (Datagram TLS) server
+   written by Eelco Huininga - 2018                        */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <cstring>		/* strerror() */
+#include <arpa/inet.h>		/* inet_ntop() */
+#include <openssl/ssl.h>	/* SSL*, SSL_new(), SSL_get_error(), SSL_set_fd(), SSL_accept(), SSL_get_cipher_version(), SSL_get_cipher_name(), SSL_get_cipher_bits(), SSL_get_verify_result(), SSL_get_peer_certificate(), SSL_read(), SSL_write(), SSL_ERROR_WANT_READ, SSL_ERROR_SYSCALL, SSL_shutdown(), SSL_free() */
+#include <openssl/engine.h>	/* ERR_get_error() */
+
+#include "libdtls.h"
+
+#define PORT	33859
+#define MAXLEN	4096
+
+static bool done;		/* To handle shutdown */
+struct sockaddr_in cliaddr;	/* Client'ss address */
+
+void hexdump(const char *string, int size);
+
+const char	*SSL_CIPHERS	= "ALL:kECDHE:!COMPLEMENTOFDEFAULT:!EXPORT:!LOW:!aNULL:!eNULL:!SSLv2:!SSLv3:!TLSv1:!kRSA:!SHA1";
+const char	*SSL_SIGALGS	= "ECDSA+SHA512:RSA+SHA512";
+const char	*DHFILE		= "dh4096.pem";
+const char	*CA		= NULL;
+const char	*CERT		= "server-cert.pem";
+const char	*PRIVKEY	= "server-key.pem";
+const char	reply[]		= "Message received loud 'n clear, Houston!\n";
+
+
+int main(int argc, char** argv) {
+	SSL_CTX*	ctx;
+	ssize_t		connfd = 0;
+	int		rx_length = 0;		/* length of message */
+	int		sock = 0;		/* Initialize our socket */
+	int		ssl_errno;		/* SSL error number */
+	socklen_t	client_len;
+	char		buff[MAXLEN];		/* SSL_read buffer */
+	char		ipAddress[INET_ADDRSTRLEN];
+	SSL*		ssl = NULL;
+
+
+	/* Initialize SSL Engine and context */
+	ctx = libdtls::ssl_initialize();
+
+	/* Await Datagram */
+	done = false;
+	while (done != true) {
+		/* Create a new socket */
+		sock = libdtls::create_socket(AF_INET, NULL, PORT);
+		printf("Listening on UDP port %d\n", PORT);
+
+		client_len = sizeof(cliaddr);
+		do {
+			connfd = recvfrom(sock, (char *)&buff, sizeof(buff), MSG_PEEK, (struct sockaddr*)&cliaddr, &client_len);
+			if (connfd < 0) {
+				if (errno != EWOULDBLOCK) {
+					printf("recvfrom() failed: %s\n", strerror(errno));
+					libdtls::ssl_cleanup(ctx);
+					exit(1);
+				}
+			}
+			if (connfd == 0) {
+				printf("Peer has performed an orderly shutdown.\n");
+				libdtls::ssl_cleanup(ctx);
+				exit(1);
+			}
+		} while (connfd < 0);
+
+		if (connect(sock, (const struct sockaddr *)&cliaddr, sizeof(cliaddr)) != 0) {
+			printf("UDP connect failed.\n");
+			libdtls::ssl_cleanup(ctx);
+			exit(1);
+		} else {
+			printf("Connected to %s:%d\n", inet_ntop(AF_INET, &(cliaddr.sin_addr), ipAddress, INET_ADDRSTRLEN), cliaddr.sin_port);
+
+			/* Create the OpenSSL object */
+			if ((ssl = SSL_new(ctx)) == NULL) {
+				libdtls::ssl_print_error("SSL_new", SSL_get_error(ssl, 0));
+				libdtls::ssl_cleanup(ctx);
+				exit(1);
+			}
+
+			/* Set the session ssl to client connection port */
+			SSL_set_fd(ssl, sock);
+
+			/* Accept the connection */
+			if (SSL_accept(ssl) != 1) {
+				libdtls::ssl_print_error("SSL_accept", SSL_get_error(ssl, 0));
+				libdtls::ssl_cleanup(ctx);
+				exit(1);
+			}
+
+			printf("%s handshake completed; secure connection established, using cipher %s (%d bits)\n", SSL_get_cipher_version(ssl), SSL_get_cipher_name(ssl), SSL_get_cipher_bits(ssl, NULL));
+
+			/* Verify the client certificate */
+			X509 *cert = SSL_get_peer_certificate(ssl);
+			if (SSL_get_verify_result(ssl) == X509_V_OK) {
+				STACK_OF(X509) *sk = SSL_get_peer_cert_chain(ssl);
+				if ((cert = SSL_get_peer_certificate(ssl)) != NULL) {
+					printf("Client certificate's subject: %s", X509_NAME_oneline(X509_get_subject_name(cert), NULL, 0));
+					printf("Client certificate's issuer: %s", X509_NAME_oneline(X509_get_issuer_name(cert), NULL, 0));
+					printf("Client certificate's signature algorithm: %i", X509_get0_tbs_sigalg(cert));
+				}
+			}
+			X509_free(cert);
+
+			bool blocking  = false;
+			do {
+				if ((rx_length = SSL_read(ssl, buff, sizeof(buff))) > 0) {
+					buff[rx_length] = 0;
+					ssl_errno = SSL_get_error(ssl, 0);
+					printf("rxlen:%i - sslerrno:%i - ERR_get_error:%lu - errno:%i %s - SSLpending:%i\n", rx_length, ssl_errno, ERR_get_error(), errno, strerror(errno), SSL_pending(ssl));
+					printf("Received 0x%04x bytes:\n", rx_length);
+					hexdump(buff, rx_length);
+					if (SSL_write(ssl, reply, sizeof(reply)) < 0) {
+						libdtls::ssl_print_error("SSL_write", SSL_get_error(ssl, 0));
+						libdtls::ssl_cleanup(ctx);
+						exit(1);
+					} else {
+						printf("Transmitted 0x%04x bytes:\n", (int)sizeof(reply));
+						hexdump(reply, sizeof(reply));
+						blocking = false;
+					}
+				} else {
+					ssl_errno = SSL_get_error(ssl, 0);
+					printf("rxlen:%i - sslerrno:%i - ERR_get_error:%lu - errno:%i %s - SSLpending:%i\n", rx_length, ssl_errno, ERR_get_error(), errno, strerror(errno), SSL_pending(ssl));
+					if((ssl_errno != SSL_ERROR_WANT_READ) && ((ssl_errno == SSL_ERROR_SYSCALL) && (errno != EWOULDBLOCK))){
+						libdtls::ssl_print_error("SSL_read", ssl_errno);
+						libdtls::ssl_cleanup(ctx);
+						exit(1);
+					} else
+						blocking = true;
+				}
+			} while (blocking == true);
+
+			printf("Closing connection\n");
+			SSL_shutdown(ssl);
+			SSL_free(ssl);
+		}
+	}
+
+	libdtls::ssl_cleanup(ctx);
+	return 0;
+}
+
+void hexdump(const char *string, int size) {
+	int i, offset;
+
+	offset = 0;
+	while ((size - offset) > 0) {
+		printf("%04X  ", offset);
+
+		for (i = 0; i < 16; i++) {
+			if ((offset + i) < size)
+				printf("%02X ", string[offset + i]);
+			else
+				printf("   ");
+		}
+		printf(" ");
+		for (i = 0; i < 16; i++) {
+			if ((offset + i) < size) {
+				if ((string[offset + i] > 31) && (string[offset + i] < 127))
+					printf("%c", string[offset + i]);
+				else
+					printf(".");
+			}
+		}
+		printf("\n");
+		offset += 16;
+	}
+}
+
